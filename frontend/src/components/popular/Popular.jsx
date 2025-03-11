06fa291accbdef5a8fd3fa7c0be89e049e96eb4c
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-11 12:44:34 -0300

    Fix fetch URL in Popular component and add safety check for mapping over popular items

--- frontend/src/components/popular/Popular.jsx
+++ frontend/src/components/popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popular, setPopular] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/featured-women")
+    fetch("http://localhost:4000/api/featured-women")
       .then((res) => res.json())
       .then((data) => setPopular(data));
   }, []);
@@ -17,16 +17,18 @@ const Popular = () => {
       <h1>Popular For Women</h1>
       <hr />
       <div className="popular-items">
-        {popular.map((item) => (
-          <Item
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            name={item.name}
-            new_price={`$${item.new_price}`}
-            old_price={`$${item.old_price}`}
-          />
-        ))}
+        {popular &&
+          popular.length > 0 &&
+          popular.map((item) => (
+            <Item
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              new_price={`$${item.new_price}`}
+              old_price={`$${item.old_price}`}
+            />
+          ))}
       </div>
     </div>
   );
