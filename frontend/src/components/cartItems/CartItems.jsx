31172d82913d08a170e89b77b99dd1b1dd786a2f
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-13 14:47:48 -0300

    Remove DEPRECATED files

--- frontend/src/components/cartItems/CartItems.jsx
+++
@@ -1,7 +0,0 @@
-// Path: frontend/src/components/cartItems/CartItems.jsx
-// DEPRECATED: This file has been refactored into a component folder structure.
-// Please use the index.jsx file instead.
-// This file is kept for backward compatibility and will be removed in a future update.
-
-import CartItems from "./index";
-export default CartItems;
--- frontend/src/components/productDisplay/ProductDisplay.jsx
+++
@@ -1,7 +0,0 @@
-// Path: frontend/src/components/productDisplay/ProductDisplay.jsx
-// DEPRECATED: This file has been refactored into a component folder structure.
-// Please use the index.jsx file instead.
-// This file is kept for backward compatibility and will be removed in a future update.
-
-import ProductDisplay from "./index";
-export default ProductDisplay;
