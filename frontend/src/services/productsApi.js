8c2ebf002ee52d4e4461bd4cdf29e153220c2392
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-21 15:48:43 -0300

    Update routes

--- frontend/src/services/productsApi.js
+++ frontend/src/services/productsApi.js
@@ -11,7 +11,7 @@ const productsApi = {
    */
   getProducts: async (filters = {}) => {
     try {
-      const response = await apiClient.get("/api/products", {
+      const response = await apiClient.get("/api/products/all-products", {
         params: filters,
       });
       return response.data;
