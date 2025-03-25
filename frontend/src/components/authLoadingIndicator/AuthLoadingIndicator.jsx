073370e70b6e4e55e51eec0752e694e59449a940
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-25 16:28:43 -0300

    remove unused import

--- frontend/src/components/authLoadingIndicator/AuthLoadingIndicator.jsx
+++ frontend/src/components/authLoadingIndicator/AuthLoadingIndicator.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { InlineSpinner } from "../ui/SpinnerUtils";
 import "./authLoadingIndicator.css"; // Import dedicated CSS file
