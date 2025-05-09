f7a33107594b8ffe4a25c7c1d6c55e0997b794d4
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-05-09 15:18:52 -0300

    Remove old deprecated routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,8 +5,6 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 
 // Product routes
-router.post("/add-product", productController.addProduct);
-router.post("/remove-product", productController.removeProduct);
 router.get("/all-products", productController.getAllProducts);
 router.get("/newcollection", productController.getNewCollection);
 router.get("/featured-women", productController.getFeaturedWomen);
