48e9e298976c8f92b3e64bdbfa5d2ac91eba14aa
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-05-13 14:54:01 -0300

    Added extra test card info on checkout

--- frontend/src/pages/checkout/components/TestCardInfo.jsx
+++ frontend/src/pages/checkout/components/TestCardInfo.jsx
@@ -4,14 +4,26 @@ import "../styles/TestCardInfo.css";
 const TestCardInfo = () => {
   return (
     <div className="checkout-test-card-info">
-      <h4 className="checkout-test-card-title">
-        Test Card Information (For Development)
-      </h4>
+      <h4 className="checkout-test-card-title">Test Card Information</h4>
       <p className="checkout-test-card-detail">
-        Card Number: 4242 4242 4242 4242
+        <strong>Card Number:</strong> 4242 4242 4242 4242
       </p>
-      <p className="checkout-test-card-detail">Expiry: Any future date</p>
-      <p className="checkout-test-card-detail">CVC: Any 3 digits</p>
+      <p className="checkout-test-card-detail">
+        <strong>Expiry:</strong> Any future date
+      </p>
+      <p className="checkout-test-card-detail">
+        <strong>CVC:</strong> Any 3 digits
+      </p>
+      <h4 className="checkout-test-card-title">Other Test Cards</h4>
+      <p className="checkout-test-card-detail">
+        <strong>Decline:</strong> 4000 0000 0000 0002
+      </p>
+      <p className="checkout-test-card-detail">
+        <strong>Insufficient funds:</strong> 4000 0000 0000 9995
+      </p>
+      <p className="checkout-test-card-detail">
+        <strong>Error:</strong> 4000 0000 0000 0127
+      </p>
     </div>
   );
 };
